75bacebc8636ce32a54fa6e84d15fa0bc2fb04e3
muerek <70488654+muerek@users.noreply.github.com>
2022-05-28 21:52:52 +0200

    Switch runtime to .NET 6. (#6)

--- BwMelder/Pages/Admin/Login.cshtml.cs
+++ BwMelder/Pages/Admin/Login.cshtml.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
-using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
 
 namespace BwMelder.Pages.Admin
 {
