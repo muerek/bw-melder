6d0627c10360ddf28edc349782377e256d023db1
muerek <70488654+muerek@users.noreply.github.com>
2023-05-26 18:47:57 +0200

    Redesign mailing page.

--- BwMelder/Pages/Reports/Mailing.cshtml.cs
+++ BwMelder/Pages/Reports/Mailing.cshtml.cs
@@ -16,8 +16,8 @@ namespace BwMelder.Pages.Reports
             this.db = db;
         }
 
-        public IList<string> TeamCoachEmailAddresses { get; set; }
-        public IList<string> HomeCoachEmailAddresses { get; set; }
+        public IList<string> TeamCoachEmailAddresses { get; set; } = new List<string>();
+        public IList<string> HomeCoachEmailAddresses { get; set; } = new List<string>();
 
         public IList<string> AllEmailAddresses => TeamCoachEmailAddresses.Concat(HomeCoachEmailAddresses).Distinct().ToList();
 
@@ -36,6 +36,11 @@ namespace BwMelder.Pages.Reports
                 .ToListAsync();
         }
 
+        /// <summary>
+        /// Generates a mailto link addressing all given addresses as Bcc recipients.
+        /// </summary>
+        /// <param name="emailAddresses">List of email addresses.</param>
+        /// <returns>Mailto link to send an email to all addresses as Bcc recipients.</returns>
         public string GetMailtoLink(IList<string> emailAddresses)
         {
             return "mailto:?bcc=" + string.Join(",", emailAddresses);
