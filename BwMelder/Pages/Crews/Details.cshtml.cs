01db99a490ac7ba4302460b31ebae0df907d2ecc
muerek <70488654+muerek@users.noreply.github.com>
2023-05-27 12:15:19 +0200

    Move QR code generation to partial.

--- BwMelder/Pages/Crews/Details.cshtml.cs
+++ BwMelder/Pages/Crews/Details.cshtml.cs
@@ -7,34 +7,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BwMelder.Data;
 using BwMelder.Model;
+using Microsoft.AspNetCore.Http;
+using BwMelder.Extensions;
 
 namespace BwMelder.Pages.Crews
 {
     public class DetailsModel : PageModel
     {
-        private readonly BwMelderDbContext _context;
+        private readonly BwMelderDbContext db;
+        private readonly IHttpContextAccessor httpContextAccessor;
 
-        public DetailsModel(BwMelderDbContext context)
+        public DetailsModel(BwMelderDbContext db, IHttpContextAccessor httpContextAccessor)
         {
-            _context = context;
+            this.db = db;
+            this.httpContextAccessor = httpContextAccessor;
         }
 
-        public Crew Crew { get; set; }
+        public Crew? Crew { get; set; }
+        public string BaseUrl { get; set; } = string.Empty;
 
         public async Task<IActionResult> OnGetAsync(Guid id)
         {
-            Crew = await _context.Crews
+            Crew = await db.Crews
                 .AsNoTracking()
                 .Include(c => c.HomeCoach)
                 .Include(c => c.Race)
                 .Include(c => c.Athletes)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .SingleOrDefaultAsync(m => m.Id == id);
 
             if (Crew == null)
             {
                 return NotFound();
             }
+
+            BaseUrl = httpContextAccessor.HttpContext?.Request.BaseUrl() ?? string.Empty;
+
             return Page();
         }
+
+        public string GetSecretUrl() => $"{BaseUrl}Crews/Details/{Crew?.Id}";
     }
 }
