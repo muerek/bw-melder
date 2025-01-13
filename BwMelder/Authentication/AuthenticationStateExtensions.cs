28b6434b7447c17b0738d6b17d4dfe3e683752a0
muerek <70488654+muerek@users.noreply.github.com>
2025-01-13 21:48:12 +0100

    Add easy access user information in components.

---
+++ BwMelder/Authentication/AuthenticationStateExtensions.cs
@@ -0,0 +1,33 @@
+﻿using BwMelder.Shared.Dto;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BwMelder.Authentication;
+
+public static class AuthenticationStateExtensions
+{
+    /// <summary>
+    /// Reads information on the current user from the <see cref="CascadingAuthenticationState"/>.
+    /// </summary>
+    /// <param name="task"></param>
+    /// <returns>User information or null if no authenticated user.</returns>
+    public static async Task<UserContext?> GetUserContextAsync(this Task<AuthenticationState>? task)
+    {
+        if (task is not null)
+        {
+            var authenticationState = await task;
+            var user = authenticationState.User;
+
+            if (user?.Identity is not null && user.Identity.IsAuthenticated)
+            {
+                return new UserContext
+                {
+                    Role = user.FindFirstValue(ClaimTypes.Role) ?? string.Empty,
+                    ClubId = Guid.TryParse(user.FindFirstValue("ClubId"), out var guid) ? guid : null,
+                    ClubName = user.FindFirstValue("ClubName")
+                };
+            }
+        }
+        return null;
+    }
+}
