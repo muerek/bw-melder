73160c66e92100bb9121e6079b2933f4fc6da94a
muerek <70488654+muerek@users.noreply.github.com>
2025-01-13 23:03:25 +0100

    Add extension to get user context.

--- BwMelder/Authentication/AuthenticationStateExtensions.cs
+++ BwMelder/Authentication/AuthenticationStateExtensions.cs
@@ -10,7 +10,7 @@ public static class AuthenticationStateExtensions
     /// Reads information on the current user from the <see cref="CascadingAuthenticationState"/>.
     /// </summary>
     /// <param name="task"></param>
-    /// <returns>User information or null if no authenticated user.</returns>
+    /// <returns>User information; null if no authenticated user.</returns>
     public static async Task<UserContext?> GetUserContextAsync(this Task<AuthenticationState>? task)
     {
         if (task is not null)
@@ -27,6 +27,25 @@ public static class AuthenticationStateExtensions
                     ClubName = user.FindFirstValue("ClubName")
                 };
             }
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Reads information on the current user directly from the <see cref="AuthenticationStateProvider"/>.
+    /// </summary>
+    /// <param name="provider"></param>
+    /// <returns>User information; null if no authenticated user.</returns>
+    /// <remarks>
+    /// This is not the recommended approach as there is no notifications for authentication state changes.
+    /// But it could be an easier-to-use extension method if you do not care about that.
+    /// </remarks>
+    public static async Task<UserContext?> GetUserContextAsync(this AuthenticationStateProvider? provider)
+    {
+        if (provider is not null)
+        {
+            var task = provider.GetAuthenticationStateAsync();
+            return await task.GetUserContextAsync();
         }
         return null;
     }
