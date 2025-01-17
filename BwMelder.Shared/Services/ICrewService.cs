432eb51cf29671f407fe805cbc89e58fc6c16080
muerek <70488654+muerek@users.noreply.github.com>
2025-01-17 21:16:53 +0100

    Add documentation.

--- BwMelder.Shared/Services/ICrewService.cs
+++ BwMelder.Shared/Services/ICrewService.cs
@@ -7,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace BwMelder.Shared.Services;
 
+/// <summary>
+/// Represents a service to manage crews.
+/// </summary>
 public interface ICrewService
 {
+    /// <summary>
+    /// Creates a new crew.
+    /// </summary>
+    /// <param name="request">Request object specifying details like race and club of the crew.</param>
+    /// <returns>Unique ID of the new crew.</returns>
     Task<Guid> CreateCrewAsync(CreateCrewRequest request);
 }
