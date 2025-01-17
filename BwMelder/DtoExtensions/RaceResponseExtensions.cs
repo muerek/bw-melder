f3353bcf817ffe7cbd461b40e70c3c1f79cfd914
muerek <70488654+muerek@users.noreply.github.com>
2025-01-17 21:25:24 +0100

    Clean up computed properties.

---
+++ BwMelder/DtoExtensions/RaceResponseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BwMelder.Shared.Dto;
+
+namespace BwMelder.DtoExtensions;
+
+static class RaceResponseExtensions
+{
+    /// <summary>
+    /// Total number of athletes per crew in this race.
+    /// </summary>
+    internal static int AthleteCount(this RaceResponse race) => race.RowerCount + (race.Coxed ? 1 : 0);
+
+    /// <summary>
+    /// Full display name of the race.
+    /// </summary>
+    internal static string FullName(this RaceResponse race) => $"{race.Number} - {race.Name}";
+}
